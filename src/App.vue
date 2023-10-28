04a46dcd9ac34348445b90555ac86e1a8cb57d5b
juliana1390 <juliana13290@gmail.com>
2023-10-28 13:12:24 -0300

    footer added

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,17 @@
     <div class="content">
       <router-view />
     </div>
+    <FooterComp></FooterComp>
   </div>
 </template>
 <script>
 import NavBar from './components/NavBar.vue'
 import AcessibilityMenu from './components/AcessibilityMenu.vue'
+import FooterComp from './components/FooterComp.vue'
 
 export default ({
   name: 'App',
-  components: { NavBar, AcessibilityMenu }
+  components: { NavBar, AcessibilityMenu, FooterComp}
 })
 </script>
 
---
+++ src/components/FooterComp.vue
@@ -0,0 +1,165 @@
+<template>
+  <!-- Footer -->
+<footer class="footer-comp text-center text-lg-start text-muted">
+
+
+  <!-- Section: Links  -->
+  <section class="links-section">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3 text-secondary"></i>ARTESANIA
+          </h6>
+          <p>
+            Transformando sonhos em obras de arte, uma pincelada de cada vez, porque a vida é nossa tela em branco.
+          </p>
+
+          <!-- Section: Social media -->
+  <section class="socialmedia-section d-flex justify-content-center justify-content-lg-between p-4">
+    
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+  
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            A EMPRESA
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Sobre Nós</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Features</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Works</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Carreira</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            AJUDA
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Suporte</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Detalhes da Entrega</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Termos e Condições</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Política de Privacidade</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            FAQ
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Conta</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Gerenciar Entregas</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Pedidos</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Pagamentos</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            RECURSOS
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">EBook Gratuito</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Tutoriais de Uso</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">How to - Blog</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Playlist no Youtube</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="border-top text-left p-4" style="background-color: rgba(234, 183, 102, 0.025);">
+    <!-- Left -->
+      Artesania © 2023, Todos os direitos reservados
+    <!-- Left -->
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
+</template>
+
+<script>
+    export default {
+        name: 'FooterComp'
+    }
+</script>
+
+<style lang="scss" scoped>
+  .footer-comp{
+    color: $footer-color;
+  }
+  .links-section{
+    color: $letter-footer-color;
+  }
+
+  .socialmedia-section{
+    color: $letter-footer-color;
+  }
+
+</style>
