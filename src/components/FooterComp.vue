8604a88863abe433ba5d8f5675ce28ebcd07e075
juliana1390 <juliana13290@gmail.com>
2023-10-28 16:33:24 -0300

    payment methods added to footer

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -2,6 +2,17 @@
   <!-- Footer -->
 <footer class="footer-comp text-center text-lg-start text-muted">
 
+  <!--div class="subscribe-footer container text-center text-md-start mt-5">
+
+  </div><!-->
+  
+  <div class="subs-footer container-sm">
+    
+    <h3 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
+      FIQUE ATUALIZADO SOBRE NOSSAS ÚLTIMAS OFERTAS
+    </h3>
+    
+  </div>
 
   <!-- Section: Links  -->
   <section class="links-section">
@@ -11,8 +22,8 @@
         <!-- Grid column -->
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <!-- Content -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3 text-secondary"></i>ARTESANIA
+          <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
+            <i class="brand-name"></i>ARTESANIA
           </h6>
           <p>
             Transformando sonhos em obras de arte, uma pincelada de cada vez, porque a vida é nossa tela em branco.
@@ -46,7 +57,7 @@
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
             A EMPRESA
           </h6>
           <p>
@@ -67,7 +78,7 @@
         <!-- Grid column -->
         <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h6 class="ajuda text-uppercase fw-bold mb-4" style="color: #9D3207">
             AJUDA
           </h6>
           <p>
@@ -88,7 +99,7 @@
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h6 class="faq text-uppercase fw-bold mb-4" style="color: #9D3207">
             FAQ
           </h6>
           <p>
@@ -109,7 +120,7 @@
         <!-- Grid column -->
         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
           <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
             RECURSOS
           </h6>
           <p>
@@ -134,32 +145,47 @@
   <!-- Section: Links  -->
 
   <!-- Copyright -->
-  <div class="border-top text-left p-4" style="background-color: rgba(234, 183, 102, 0.025);">
+  <div class="border-top text-center p-4">
     <!-- Left -->
       Artesania © 2023, Todos os direitos reservados
     <!-- Left -->
+    
+    <!-- Right -->
+      <img src = ../assets/img/BadgeVisa.svg>
+      <img src = ../assets/img/BadgeMasterCard.svg>
+      <img src = ../assets/img/BadgePayPal.svg>
+      <img src = ../assets/img/BadgeApple.svg>
+      <img src = ../assets/img/BadgeGooglePay.svg>
+      <img src = ../assets/img/BadgeBarCode.svg>
+      <img src = ../assets/img/BadgePix.svg>
+      <img src = ../assets/img/site-seguro-google.svg>
+    <!-- Right -->
+    
+    
   </div>
+
   <!-- Copyright -->
 </footer>
 <!-- Footer -->
 </template>
 
 <script>
-    export default {
-        name: 'FooterComp'
-    }
+  export default {
+    name: 'FooterComp'
+  }
 </script>
 
 <style lang="scss" scoped>
   .footer-comp{
-    color: $footer-color;
-  }
-  .links-section{
-    color: $letter-footer-color;
+    background-color: $footer-color;   
   }
 
-  .socialmedia-section{
-    color: $letter-footer-color;
+  .ccBanner{
+    flex-direction: row-reverse;
   }
 
+  .subs-footer{
+    background-color: footer-color2;
+  }
+  
 </style>
