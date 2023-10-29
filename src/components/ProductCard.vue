87a5d3702616145b156b3d23ad26c0dea96c4846
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-10-29 17:12:57 -0300

    card with rating and price

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,13 +3,18 @@
         <img src="../assets/img/products/product-1.png" class="card-img-top" alt="...">
         <div class="card-body">
             <h3 class="card-text">Fragmentos da Realidade</h3>
+            <star-rating :inline="true" :star-size="20" :read-only="true" :show-rating="true" :rating="4.5" :round-start-rating="false"></star-rating>
+            <p class="card-value">R$130</p>
         </div>
     </div>
 </template>
 
 <script>
+import StarRating from 'vue-star-rating'
+
 export default {
     name: 'ProductCard',
+    components: { StarRating }
 }
 </script>
 
@@ -18,5 +23,10 @@ export default {
     font-weight: 1000;
     font-size: large;
     color: $product-card-title;
+}
+.card-value{
+    font-weight: 1000;
+    font-size: large;
+    color: $product-card-price;
 }
 </style>
