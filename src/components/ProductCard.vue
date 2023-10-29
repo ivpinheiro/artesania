56bab24db926cf6c6e2a82564b22f5242b4b7556
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-10-29 16:39:46 -0300

    card product in progress

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,9 +1,8 @@
 <template>
     <div class="card" style="width: 18rem;">
-        <img src="" class="card-img-top" alt="...">
+        <img src="../assets/img/products/product-1.png" class="card-img-top" alt="...">
         <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
+            <h3 class="card-text">Fragmentos da Realidade</h3>
         </div>
     </div>
 </template>
@@ -14,4 +13,10 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.card-text {
+    font-weight: 1000;
+    font-size: large;
+    color: $product-card-title;
+}
+</style>
