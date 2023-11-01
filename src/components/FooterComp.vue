8312444a05480a74425dbb1d888fd66b557267db
juliana1390 <juliana13290@gmail.com>
2023-11-01 00:45:35 -0300

    'subscribe' adjustments (footer)

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,21 +1,33 @@
 <template>
+  <!-- Footer -->
+<footer class="footer-comp text-center text-lg-start text-muted" id="footer">
+
+  <!--div class="subscribe-footer container text-center text-md-start mt-5">
+
+  </div><!-->
+  
+  
+  
+    
   <!-- Grid container -->
-  <div class="subs-footer container p-3" style="transform: translateY(100px);">
+  <div class="subs-footer container p-2">
+    
+    <!-- Section: Form -->
     <section class="">
       <form action="">
         <!--Grid row-->
         <div class="row d-flex justify-content-around">
-
+        
           <!--Grid column-->
           <div class="col-lg-6" style="color:#FFFFFF">
-            <h3 class="subscribe-offers fw-bold pt-4">
+            <h2 class="subscribe-offers fw-bold pt-3">
               FIQUE ATUALIZADO SOBRE NOSSAS ÚLTIMAS OFERTAS
-            </h3>
+            </h2>
           </div>
           <!--Grid column-->
 
           <!--Grid column-->
-          <div class="input-comp col-sm-3 pt-4">
+          <div class="input-comp col-sm-3">
             <!-- Email input -->
             <span class="text-email">Digite seu e-mail</span>
             <div class="input-group flex-nowrap mb-2">
@@ -24,8 +36,8 @@
                 <input type="text" class="form-control" placeholder="" aria-label="Digite seu e-mail"
                 aria-describedby="addon-wrapping" style="border:none">
               </div>              
-            <button type="" class="btn btn-outline-light mb-4" style="border-radius: 30px; width: 100%;">
-              Inscreva-se para receber novidades
+            <button type="" class="btn btn-outline-light mb-2" style="border-radius: 30px; width: 100%;">
+              Inscreva-se e receba novidades
             </button>
           </div>
           <!--Grid column-->
@@ -34,203 +46,201 @@
         <!--Grid row-->
       </form>
     </section>
+    <!-- Section: Form --> 
   </div>
+    
 
-  <!-- Footer -->
-  <footer class="footer-comp text-center text-lg-start text-muted" id="footer" style="padding-top: 130px;">
-    <!-- Section: Links  -->
-    <section class="links-section">
-      <div class="container text-center text-md-start mt-6">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
-              <span class="brand-name">ARTESANIA</span>
-            </h6>
-            <p class="text-art">
-              Transformando sonhos em obras de arte, uma pincelada de cada vez, porque a vida é nossa tela em branco.
-            </p>
+  <!-- Section: Links  -->
+  <section class="links-section">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
+            <i class="brand-name"></i>ARTESANIA
+          </h6>
+          <p>
+            Transformando sonhos em obras de arte, uma pincelada de cada vez, porque a vida é nossa tela em branco.
+          </p>
 
-            <!-- Section: Social media -->
-            <section class="socialmedia-section d-flex justify-content-center justify-content-lg-between p-4">
-
-              <!-- Right -->
-              <div>
-                <a href="" class="me-4 link-secondary">
-                  <i class="fab fa-facebook-f"></i>
-                </a>
-                <a href="" class="me-4 link-secondary">
-                  <i class="fab fa-twitter"></i>
-                </a>
-                <a href="" class="me-4 link-secondary">
-                  <i class="fab fa-google"></i>
-                </a>
-                <a href="" class="me-4 link-secondary">
-                  <i class="fab fa-instagram"></i>
-                </a>
-
-              </div>
-              <!-- Right -->
-            </section>
-            <!-- Section: Social media -->
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
-              A EMPRESA
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Sobre Nós</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Features</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Works</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Carreira</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="ajuda text-uppercase fw-bold mb-4" style="color: #9D3207">
-              AJUDA
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Suporte</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Detalhes da Entrega</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Termos e Condições</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Política de Privacidade</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="faq text-uppercase fw-bold mb-4" style="color: #9D3207">
-              FAQ
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">Conta</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Gerenciar Entregas</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Pedidos</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Pagamentos</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
-              RECURSOS
-            </h6>
-            <p>
-              <a href="#!" class="text-reset">EBook Gratuito</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Tutoriais de Uso</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">How to - Blog</a>
-            </p>
-            <p>
-              <a href="#!" class="text-reset">Playlist no Youtube</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
+          <!-- Section: Social media -->
+  <section class="socialmedia-section d-flex justify-content-center justify-content-lg-between p-4">
+    
+    <!-- Right -->
+    <div>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-facebook-f"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-google"></i>
+      </a>
+      <a href="" class="me-4 link-secondary">
+        <i class="fab fa-instagram"></i>
+      </a>
+  
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
         </div>
-        <!-- Grid row -->
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
+            A EMPRESA
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Sobre Nós</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Features</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Works</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Carreira</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="ajuda text-uppercase fw-bold mb-4" style="color: #9D3207">
+            AJUDA
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Suporte</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Detalhes da Entrega</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Termos e Condições</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Política de Privacidade</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="faq text-uppercase fw-bold mb-4" style="color: #9D3207">
+            FAQ
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Conta</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Gerenciar Entregas</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Pedidos</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Pagamentos</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+        
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4" style="color: #9D3207">
+            RECURSOS
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">EBook Gratuito</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Tutoriais de Uso</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">How to - Blog</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Playlist no Youtube</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
       </div>
-    </section>
-    <!-- Section: Links  -->
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
 
-    <!-- Copyright -->
-
-    <div class="text-center p-4">
-      <hr />
+  <!-- Copyright -->
+  
+  <div class="row d-flex justify-content-around">
+    <hr />
+    <div class="col-lg">
       <!-- Left -->
       Artesania © 2023, Todos os direitos reservados
-      <!-- Left -->
-
-      <!-- Right -->
-      <img src=../assets/img/footer/BadgeVisa.svg>
-      <img src=../assets/img/footer/BadgeMasterCard.svg>
-      <img src=../assets/img/footer/BadgePayPal.svg>
-      <img src=../assets/img/footer/BadgeApple.svg>
-      <img src=../assets/img/footer/BadgeGooglePay.svg>
-      <img src=../assets/img/footer/BadgeBarCode.svg>
-      <img src=../assets/img/footer/BadgePix.svg>
-      <img src=../assets/img/footer/site-seguro-google.svg>
-      <!-- Right -->
-
-
+    <!-- Left -->
     </div>
+    
+    <div class="col-lg-5 col-sm-4">
+    <!-- Right -->
+      <img src = ../assets/img/footer/BadgeVisa.svg>
+      <img src = ../assets/img/footer/BadgeMasterCard.svg>
+      <img src = ../assets/img/footer/BadgePayPal.svg>
+      <img src = ../assets/img/footer/BadgeApple.svg>
+      <img src = ../assets/img/footer/BadgeGooglePay.svg>
+      <img src = ../assets/img/footer/BadgeBarCode.svg>
+      <img src = ../assets/img/footer/BadgePix.svg>
+      
+    <!-- Right -->
+    </div>
+    <div class="col-lg-2">
+      <img src = ../assets/img/footer/site-seguro-google.svg>
+    </div>
+    
+  </div>
 
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
 </template>
 
 <script>
-export default {
-  name: 'FooterComp'
-}
+  export default {
+    name: 'FooterComp'
+  }
 </script>
 
 <style lang="scss" scoped>
-.footer-comp {
-  background-color: $footer-color;
-}
+  .footer-comp{
+    background: linear-gradient(white 15%, 0%, $footer-color 85%);
+  }
 
-.ccBanner {
-  flex-direction: row-reverse;
-}
+  .ccBanner{
+    flex-direction: row-reverse;
+  }
 
-.subs-footer {
-  background-color: $footer-color2;
-  border-radius: 20px;
-  padding: 25px;
+  .subs-footer{
+    background-color: $footer-color2;
+    border-radius: 20px;
+    padding: 25px;
 
-}
+  }
 
-.btn-subs {
-  background-color: #FFFFFF;
-  border-radius: 20px;
-}
-.brand-name{
-  display: flex;
-  font-size: 50px;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin: 0px;
-  padding: 0px;
-}
-.text-email{
+  .btn-subs{
+    background-color: #FFFFFF;
+    border-radius: 20px;
+  }
+  
+  .text-email{
   display: flex;
   text-align: center;
   justify-content: center;
@@ -240,5 +250,6 @@ export default {
   margin: 0;
   color:#FFFFFF;
   font-weight: 900;
-}
+  }
+
 </style>
