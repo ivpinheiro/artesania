2ae8e6051448ae407a595e551e1cb3f2f4dc9586
Thamysis <thamyres.tss2468@gmail.com>
2023-11-01 17:39:23 -0300

    improve reponsivity 2

--- src/main.js
+++ src/main.js
@@ -1,45 +1,49 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-/** 
+/**
  * Font Awesome Icon
  */
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css'
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
 
-/** 
+/**
  * Bootstrap CSS, JS
  */
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
-import './styles.css'
+import "./styles.css";
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
 
 function fontSizeController() {
-    const increaseFontButton = document.getElementById("increase-font-button")
-    const decreaseFontButton = document.getElementById("decrease-font-button")
-    increaseFontButton.addEventListener("click", function () {
-        const elementsWithTextClass = document.querySelectorAll("p, h1, h2, h3, h4, span, .badge, .buttonPrimary, .btn")
-        elementsWithTextClass.forEach(function (element) {
-            const currentFontSize = window.getComputedStyle(element).fontSize
-            const currentSizeNumeric = parseFloat(currentFontSize)
-            const newSize = currentSizeNumeric + 2
-            element.style.fontSize = newSize + "px"
-        })
-    })
+  const increaseFontButton = document.getElementById("increase-font-button");
+  const decreaseFontButton = document.getElementById("decrease-font-button");
+  increaseFontButton.addEventListener("click", function () {
+    const elementsWithTextClass = document.querySelectorAll(
+      "p, h1, h2, h3, h4, h5, h6, span, .badge, .buttonPrimary, .btn"
+    );
+    elementsWithTextClass.forEach(function (element) {
+      const currentFontSize = window.getComputedStyle(element).fontSize;
+      const currentSizeNumeric = parseFloat(currentFontSize);
+      const newSize = currentSizeNumeric + 2;
+      element.style.fontSize = newSize + "px";
+    });
+  });
 
-    decreaseFontButton.addEventListener("click", function () {
-        const elementsWithTextClass = document.querySelectorAll("p, h1, h2, h3, h4, h5, h6, span, .badge, .buttonPrimary, .btn")
-        elementsWithTextClass.forEach(function (element) {
-            const currentFontSize = window.getComputedStyle(element).fontSize
-            const currentSizeNumeric = parseFloat(currentFontSize)
-            const newSize = currentSizeNumeric - 2
-            element.style.fontSize = newSize + "px"
-        })
-    })
+  decreaseFontButton.addEventListener("click", function () {
+    const elementsWithTextClass = document.querySelectorAll(
+      "p, h1, h2, h3, h4, h5, h6, span, .badge, .buttonPrimary, .btn"
+    );
+    elementsWithTextClass.forEach(function (element) {
+      const currentFontSize = window.getComputedStyle(element).fontSize;
+      const currentSizeNumeric = parseFloat(currentFontSize);
+      const newSize = currentSizeNumeric - 2;
+      element.style.fontSize = newSize + "px";
+    });
+  });
 }
 
-fontSizeController()
+fontSizeController();
