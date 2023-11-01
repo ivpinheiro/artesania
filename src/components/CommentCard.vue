7418664d6171357c42b2a958ece9813bf3f4e719
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-11-01 19:02:50 -0300

    fix scoped css styles

--- src/components/CommentCard.vue
+++ src/components/CommentCard.vue
@@ -54,7 +54,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .card {
     //height: 100%;
     //align-content: center;
--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -60,7 +60,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #card-title {
     font-family: Inter;
     font-size: x-large;
