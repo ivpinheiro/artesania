7ea2a714ac26900848150d7059028338aa86413e
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-11-02 20:31:43 -0300

    ready for build

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 /**
  * Font Awesome import  
  * */ 
-import FontAwesomeIcon from "@/utilities/fontawesome-icons.ts";
+import FontAwesomeIcon from "@/utilities/fontawesome-icons";
 
 const app = createApp(App)
 
@@ -24,33 +24,3 @@ app.use(router)
 app.use(bootstrap)
 app.component("FontAwesomeIcon", FontAwesomeIcon)
 app.mount('#app')
-
-function fontSizeController() {
-    const increaseFontButton = document.getElementById("increase-font-button");
-    const decreaseFontButton = document.getElementById("decrease-font-button");
-    increaseFontButton.addEventListener("click", function () {
-      const elementsWithTextClass = document.querySelectorAll(
-        "a, p, h1, h2, h3, h4, h5, h6, span, .badge, .buttonPrimary, .btn"
-      );
-      elementsWithTextClass.forEach(function (element) {
-        const currentFontSize = window.getComputedStyle(element).fontSize;
-        const currentSizeNumeric = parseFloat(currentFontSize);
-        const newSize = currentSizeNumeric + 2;
-        element.style.fontSize = newSize + "px";
-      });
-    });
-  
-    decreaseFontButton.addEventListener("click", function () {
-      const elementsWithTextClass = document.querySelectorAll(
-        "a, p, h1, h2, h3, h4, h5, h6, span, .badge, .buttonPrimary, .btn"
-      );
-      elementsWithTextClass.forEach(function (element) {
-        const currentFontSize = window.getComputedStyle(element).fontSize;
-        const currentSizeNumeric = parseFloat(currentFontSize);
-        const newSize = currentSizeNumeric - 2;
-        element.style.fontSize = newSize + "px";
-      });
-    });
-  }
-  
-  fontSizeController();
--- src/services/ElementService.ts
+++ src/services/ElementService.ts
@@ -10,29 +10,4 @@ export class ElementService {
         let dataURL = `${this.serverURL}/products`;
         return axios.get(dataURL);
     }
-    static getAllElements(){
-        let dataURL = `${this.serverURL}/elements`;
-        return axios.get(dataURL);
-    }
-    static getElement(elementId){
-        let dataURL = `${this.serverURL}/elements/${elementId}`;
-        return axios.get(dataURL);
-    }
-    static createElement(element){
-        let dataURL = `${this.serverURL}/elements/`;
-        return axios.post(dataURL, element);
-    }
-    static updateElement(element, elementId){
-        let dataURL = `${this.serverURL}/elements/${elementId}`;
-        return axios.put(dataURL, element);
-    }
-    static deleteElement(elementId){
-        let dataURL = `${this.serverURL}/elements/${elementId}`;
-        return axios.delete(dataURL);
-    }
-    static getAllGroups(element){
-        let groupId = element.groupId;
-        let dataURL = `${this.serverURL}/groups/${groupId}`;
-        return axios.get(dataURL);
-    }
 }
