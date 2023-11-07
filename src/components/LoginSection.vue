66eb6911f655e23ac9942ddffcbf14d7cf39188d
LuizHBB <luiz.belorio@usp.br>
2023-11-07 17:22:47 -0300

    Sign page incompleted

---
+++ src/components/LoginSection.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card border-0">
+        <div class="login-info">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label" id="lblUser"><b>Usuário</b></label>
+                <input type="text" class="form-control" id="inputUser" placeholder="@usuario">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label" id="lblPassword"><b>Senha</b></label>
+                <input type="password" class="form-control">
+            </div>
+        </div>
+        <button class="buttonPrimary">Entrar</button>
+        
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'LoginSection'
+    }
+</script>
+
+<style lang="scss" scoped>
+.card{
+    padding: 10% 10% 10% 10%;
+    height: 100%;
+    align-content: center;
+    justify-content: center;
+    background-color: #fff;
+    border-radius: 2%;
+}
+
+.card .buttonPrimary{
+    background-color: #9D3207;
+    margin-top: 20px;
+    border: 0px;
+    border-radius: 62px;
+    width: 210px;
+    height: 52px;
+    color: #fff;
+    font-family: 'Inter';
+    font-weight: 600;
+}
+
+#inputUser{
+    border-bottom-style: solid #000;
+}
+
+</style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,13 +1,47 @@
-<template lang="html">
-    <div>
-        texto!
+<template>
+    <div class="login-background">
+        <div class="firstSection">
+            <LoginSection></LoginSection>
+        </div>
+        <div class="secondSection">
+            <AboutSection></AboutSection>
+        </div>
     </div>
 </template>
+
 <script>
+
+import LoginSection from "../components/LoginSection.vue"
+import AboutSection from "../components/AboutSection.vue"
+
 export default {
-    
+    name: 'loginPage',
+    components:{
+        LoginSection,
+        AboutSection
+    }
 }
 </script>
 <style lang="scss">
     
+.login-background{
+    background-image: url("./../assets/img/banner/login-background.svg");
+    width: 100%;
+    height: 816px;
+    display: flex;
+    align-items: center;
+}
+
+.firstSection{
+    height: 100%;
+    width: 60%;
+    padding: 8% 5% 8% 5%;
+}
+
+.secondSection{
+    height: 100%;
+    width: 40%;
+    
+}
+
 </style>
