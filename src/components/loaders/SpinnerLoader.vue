1df781adc3cb3a56060bc47dc5c301d1407bd0ed
Ivan Barbosa Pinheiro <ivanpinheiro@outlook.com>
2023-11-21 01:04:49 -0300

    fix path loader to the build

--- src/components/loaders/SpinnerLoader.vue
+++ src/components/loaders/SpinnerLoader.vue
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <img src="../assets/img/loading.gif" alt="loader" class="loader-img d-block m-auto">
+                <img src="@/assets/img/loading.gif" alt="loader" class="loader-img d-block m-auto">
             </div>
         </div>
     </div>
