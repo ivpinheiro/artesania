a6ef475d14a63fbf177d91c87dd6ddb3a0aa463e
LuizHBB <luiz.belorio@usp.br>
2023-11-24 20:23:47 -0300

    payment page dont load component PaymentMethodSection

---
+++ src/components/sections/PaymentMethodSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="pay-container">
+        <p>Metodos de Pagamento</p>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'PaymentMethod'
+}
+
+</script>
+
+<style lang="css" scoped>
+
+.pay-container{
+    background-color: grey;
+}
+
+</style>
--- src/views/PaymentPage.vue
+++ src/views/PaymentPage.vue
@@ -1,14 +1,34 @@
 <template>
-
+    <div class="breadcrumbs">
+        <p> ... > Meu Painel > Pedidos > <span class="highlight">Encomenda Nº123</span></p>
+    </div>
+    <div class="pay-conteudo">
+        <PaymentMethodSection></PaymentMethodSection>
+    </div>
 </template>
 
 <script>
 
-export default{
+import PaymentMethod from "@/components/sections/PaymentMethodSection.vue"
+
+export default {
     name: 'paymentPage',
+    components: {
+        PaymentMethod,
+    }
 }
 
 </script>
 
 <style lang="css" scoped>
+
+.breadcrumbs {
+    margin: 20px 8%;
+}
+
+.pay-conteudo{
+    height: 100%;
+    width: 100%;
+}
+
 </style>
