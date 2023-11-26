0c0f4b7b75e42bcc40af08770c01b37f2f8ce702
LuizHBB <luiz.belorio@usp.br>
2023-11-26 16:56:56 -0300

    paymentConfirm page add

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import SignPage from '@/views/SignInPage.vue'
 import ArtistsGallery from '@/views/ArtistsGallery.vue'
 import PaymentPage from '@/views/PaymentPage.vue'
 import ProductPage from '@/views/ProductPage.vue'
+import ConfirmPaymentPage from '@/views/ConfirmPaymentPage.vue'
 import { authMiddleWare } from '@/middlewares/AuthMiddleWare'
 
 const router = createRouter({
@@ -67,6 +68,11 @@ const router = createRouter({
       meta: {
         breadcrumb: 'Obra'
       }
+    },
+    {
+      path: '/confirmPayment',
+      name: 'confirmPayment',
+      component: ConfirmPaymentPage,
     }
   ]
 })
---
+++ src/views/ConfirmPaymentPage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="content">
+        <div class="confirm-icon">
+            <img src="@/assets/confirmPayIcon.svg" alt="Icone de Confirmação de compra">
+        </div>
+        <div class="confirm-text">
+            <h3>Pagamento Confirmado</h3><br>
+            <p>Sua compra foi efetuada com sucesso!</p>
+            Um email foi enviado para meuemail@gmeuemail.com com detalhees do pedido
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'confirmPayment'
+}
+
+</script>
+<style lang="css" scoped>
+
+.content{
+    color: #5a5a5a;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+}
+
+.confirm-text{
+    padding: 2rem 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+</style>
