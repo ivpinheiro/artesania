45088850167276ab2f97de407e9cb0a7d0b03f96
LuizHBB <luiz.belorio@usp.br>
2023-12-01 19:33:16 -0300

    responsive login page

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="login-section">
         <div class="firstSection">
+            <div class="logo-mobile">
+                <RouterLink to="/" class="text-decoration-none">
+                    <img src="@/assets/img/navbar/artesania-logo-old.svg" alt="Artesania" class="image-about" />
+                    <span class="logo-name contrast">ARTESANIA</span>
+                </RouterLink>
+            </div>
             <LoginSection></LoginSection>
         </div>
 
@@ -29,20 +35,55 @@ export default {
 </script>
 <style lang="scss" scoped>
 .login-section {
-    display: flex;
+    display: grid;
     background-image: url("@/assets/img/banner/login-background.svg");
-    overflow: hidden;
+    grid-template-columns: 3fr 1fr;
+    width: 100vw;
+}
+
+.logo-mobile{
+    display: none;
 }
 
 .firstSection {
-    width: 70%;
+    padding: 5rem 5rem;
 }
 
-.secondSection {
-    width: 30%;
+
+@media(max-width: 768px){
+    .login-section{
+        grid-template-columns: 1fr;
+    }
+
+    .image-about{
+        margin-left: 25%;
+    }
+    .card{
+        margin: 0.5rem;
+    }
+    .logo-mobile{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: whitesmoke;
+        img{
+            width: 8rem;  
+        }
+    }
+
+    .logo-name {
+        display: flex;
+        font-family: Inter;
+        font-weight: 1000;
+        font-size: 38px;
+        color: #9D3207;
+        padding-left: 0;
+    }
+    .firstSection{
+        
+        padding: 0;
+    }
 }
 
-.firstSection {
-    padding: 10rem 10rem;
-}
 </style>
