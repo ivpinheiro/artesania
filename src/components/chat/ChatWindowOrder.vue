4486880a2237716212f96f7b8540ff09b77c0a42
MateusBezerraa <mateus98vinicius@gmail.com>
2023-12-01 11:47:19 -0300

    Finish OrderPage

---
+++ src/components/chat/ChatWindowOrder.vue
@@ -0,0 +1,165 @@
+<template>
+    <div class="contrast">
+        <div class="chat-container">
+            <div class="message-list">
+                <!-- Lista de mensagens -->
+                <div v-for="(message, index) in messages" :key="index" class="message" :class="{ 'user': message.sender === 'user', 'artist': message.sender === 'artist' }">
+                    <div class="message-content">{{ message.content }}</div>
+                
+                    <img v-if="message.sender === 'user'" src="@/assets/img/artists/ApoWhang.svg" class="profile-image user-profile" alt="User" />
+
+                    <img v-if="message.sender === 'artist'" src="@/assets/img/artists/MiraMaroni.svg" class="profile-image artist-profile" alt="Artist" />
+                </div>
+            </div>
+
+
+            <div class="message-input">
+                <img src="@/assets/img/chat/chat_microfone.svg" alt="imagem de um microfone" />
+                <img src="@/assets/img/chat/chat_image_upload.svg" alt="simbolo de upload de imagem" />
+
+                <input v-model="newMessage" type="text" placeholder="Escreva sua mensagem aqui..." @keyup.enter="sendUserMessage" />
+                <button @click="sendUserMessage">Enviar</button>
+            </div>
+        </div>
+    </div>
+  </template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        newMessage: '',
+        messages: []
+      };
+    },
+    methods: {
+      sendUserMessage() {
+        if (this.newMessage.trim()) {
+          this.messages.push({
+            content: this.newMessage,
+            sender: 'user'
+          });
+          this.newMessage = ''; 
+        }
+      },
+    }
+  };
+</script>
+  
+<style scoped>
+  .chat-container {
+    background-color: #f0eeed;
+    border-radius: 5%;
+    padding: 1rem;
+    margin: 1% auto 10%;
+    border: 0px;
+    height: auto;
+    color: #fff;
+    font-weight: 600;
+
+    @media (max-width: 780px) {
+        margin: 0%;
+        overflow: hidden;   
+    }
+    @media (max-width: 580px) {
+        margin: 0%;
+    }
+  }
+
+.message-list {
+  height: 400px;
+  overflow-y: auto;
+  background: #ececec; 
+}
+
+.message {
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    margin: 5px;
+}
+
+.message-content {
+    max-width: 70%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px #ccc;
+    color: black;
+    word-wrap: break-word;
+    border-radius: 20px;
+    background: white;
+}
+
+.profile-image {
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+
+.user{
+  justify-content: flex-end;
+}
+
+.user-message{
+  justify-content: flex-end;
+}
+
+.artist{
+  justify-content: flex-start;
+}
+
+.artist-message{
+  justify-content: flex-start;
+}
+
+.user-profile {
+  order: 2; /* Muda a ordem visualmente para aparecer depois da mensagem */
+  margin-left: 10px;
+}
+
+.artist-profile {
+  order: -1; 
+  margin-right: 10px;
+}
+
+
+.message-input {
+  display: flex;
+  padding: 10px;
+  background: #f0eeed; 
+}
+
+.message-input input {
+  flex-grow: 1;
+  margin-right: 5px;
+  border-radius: 20px;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+.message-input button {
+  padding: 10px 20px;
+  border-radius: 20px;
+  background-color: #9D3207;
+  color: white;
+  border: none;
+}
+
+.icons {
+  display: flex;
+}
+
+.icon {
+  margin-right: 10px;
+}
+
+.high-contrast {
+  color: white;
+  background: black;
+}
+
+.high-contrast-text {
+  color: white;
+}
+</style>
--- src/views/OrderPage.vue
+++ src/views/OrderPage.vue
@@ -1,22 +1,86 @@
 <template>
-    <BreadCrumbComponent></BreadCrumbComponent>
+    <div class="breadcrumbs">
+        <BreadCrumbComponent></BreadCrumbComponent>
+    </div>
     <OrderOptionsTable></OrderOptionsTable>
+    <Div class="Chat">
+        <h1 class="title no-margin">Detalhes da Encomenda</h1> 
+        <ChatWindow></ChatWindow>
+    </Div>
+
+
+    <div class="confirm-area">
+            <button class="buttonPrimary-confirmar contrast">Confirmar</button>
+    </div>
 </template>
 
 <script>
 
 import BreadCrumbComponent from '@/components/breadcrumbs/BreadCrumbComponent.vue'
-import  OrderOptionsTable from '@/components/tables/OrderOptionsTable.vue';
+import OrderOptionsTable from '@/components/tables/OrderOptionsTable.vue';
+import ChatWindow from '@/components/chat/ChatWindowOrder.vue';
 
 export default {
     name: 'OrderPage',
     components: {
         OrderOptionsTable,
-        BreadCrumbComponent
+        BreadCrumbComponent,
+        ChatWindow
     }
 }
 
 </script>
 
 <style lang="css" scoped>
+
+.breadcrumbs {
+    margin: 20px 8%;
+    padding-bottom: 1rem;
+}
+.confirm-area {
+    display: flex;
+    justify-content: center;
+    padding: 1rem 0 5rem 0;
+}
+
+.buttonPrimary-confirmar {
+    background-color: #9D3207;
+    border: 0px;
+    border-radius: 62px;
+    width: 25vh;
+    height: 40px;
+    color: #fff;
+    font-weight: 600;
+}
+
+.title {
+    color: #9D3207;
+    font-family: Inter;
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+    margin-top: 20px;
+    padding: 1rem;
+    align-self: self-start;
+    @media (max-width: 1060px) {
+        font-size: 26px;
+    }
+}
+.no-margin{
+    margin:0px !important;
+}
+
+.Chat {
+    display: flex;
+    flex-direction: column;
+    margin: 0% 15%;
+    @media (max-width: 780px) {
+        margin: 0% 5%;
+        overflow: hidden;   
+    }
+    @media (max-width: 580px) {
+        margin: 0% 2%;
+    }
+}
 </style>
